1aa83a3dab096aa21c963dd2389ec65455a37bf0
suricatta <suricatta1998@outlook.com>
2020-11-13 17:15:07 +0800

    update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -61,9 +61,7 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
-    maxChunkSize: 300000
-  },
-  generate: {
-    dir: 'public'
+    hints: false,
+    maxAssetSize: 200000
   }
 }
